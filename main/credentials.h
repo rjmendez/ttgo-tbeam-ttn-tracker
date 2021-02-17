c8718b4e8af76760f2152fe1ca7a199196a72ba6
rjmendez <rjmendez@knobgoblin.org>
2021-02-16 19:16:50 -0600

    Fixing what I broke in the credentials file.

--- main/credentials.h
+++ main/credentials.h
@@ -7,8 +7,8 @@ Credentials file
 #pragma once
 
 // Only one of these settings must be defined
+//#define USE_ABP
 #define USE_OTAA
-//#define USE_OTAA
 
 #ifdef USE_ABP
 
@@ -28,16 +28,16 @@ Credentials file
     // first. When copying an EUI from ttnctl output, this means to reverse
     // the bytes. For TTN issued EUIs the last bytes should be 0x00, 0x00,
     // 0x00.
-    static const u1_t PROGMEM APPEUI[8]  = { 0x1C, 0xD0, 0x02, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 };
+    static const u1_t PROGMEM APPEUI[8]  = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
     // This should also be in little endian format (lsb), see above.
     // Note: You do not need to set this field, if unset it will be generated automatically based on the device macaddr
-    static u1_t DEVEUI[8]  = { 0x91, 0xB9, 0x02, 0x07, 0x68, 0xC1, 0x24, 0x00 };
+    static u1_t DEVEUI[8]  = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
     // This key should be in big endian format (msb) (or, since it is not really a
     // number but a block of memory, endianness does not really apply). In
     // practice, a key taken from ttnctl can be copied as-is.
     // The key shown here is the semtech default key.
-    static const u1_t PROGMEM APPKEY[16] = { 0x95, 0x41, 0x51, 0x8D, 0xED, 0xEC, 0xC5, 0x23, 0x8B, 0x5C, 0x18, 0x32, 0x82, 0xDD, 0xB2, 0x0D };
+    static const u1_t PROGMEM APPKEY[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
 #endif
